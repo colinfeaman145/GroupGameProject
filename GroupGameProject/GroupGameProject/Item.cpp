bf8a0482feecb4a560bc608cbc094dac45a1f983
Leon <xklaky@gmail.com>
2026-06-05 20:02:27 +1200

    fixed bug: canCollide had no effect on item fix: added canCollide check in Item.cpp file

--- GroupGameProject/GroupGameProject/Item.cpp
+++ GroupGameProject/GroupGameProject/Item.cpp
@@ -46,6 +46,9 @@ void Item::HandleCollision(Collidable* other, Vector2 penetration) {
 	if (isToBeDeleted) {
 		return;
 	}
+	if (!canCollide) {
+		return;
+	}
 
 	if (auto entity = dynamic_cast<Attackable*>(other)) {
 		entity->AddItem(id, 1);
